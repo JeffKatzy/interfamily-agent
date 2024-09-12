6a02eaf725f1c663079537ad4930c679193f90b3
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2024-09-12 10:49:20 -0400

    update workflow and prompts

--- domain/models/part.py
+++ domain/models/part.py
@@ -5,11 +5,12 @@ class Part(BaseModel):
     part: str = Field("",
         description="A feeling, struggle, thought pattern, or part they encounter.")
     aware_of_part: str = Field("",
-        description="Physical feeling of part or the sense of the part")
+        description="Physical feeling of part or the sense of the part.  For example, a pain the neck or tightness in the jaw.")
     feeling_to_part: str = Field("",
         description="Emotion or response towards the primary part")
     achieved_unblending: bool = Field("", description = """Achieved unblending if feels compassion or warmth towards the part.
     Did not achieve if feels negative towards the part.""")
+    has_compassion_towards_part: bool = Field("", description = """Has the user expressed a degree of compassion or understanding towards the part.""")
     shared_feelings: bool = Field("", description = """Was the user able to share its compassion towards the part.""")
     
     
--- domain/workflows/part_workflow.py
+++ domain/workflows/part_workflow.py
@@ -8,16 +8,18 @@ class PartWorkflow(BaseWorkflow):
 
     find_part: WField = WField(prompt="Ask if there's a feeling, struggle, thought pattern, or part they need help with.",
         skip=lambda view: bool(view._model.part))
-    assess_awareness: WField = WField(prompt = "Thank them, mirror using parts language.  Then ask if they're aware of this part and how they sense or are aware of the part.",
+    assess_awareness: WField = WField(prompt = "Thank them, mirror using parts language.  Then ask if they're aware of this part and how they sense or are aware of the part in their body.",
                         skip=lambda view: bool(view._model.aware_of_part))
-    ask_feeling_towards_part: WField = WField(prompt = "Ask how they feel toward the target part.",
+    ask_feeling_towards_part: WField = WField(prompt = "Ask how they feels towards the target part.  For example, are they ok with the part being there?",
                                 skip=lambda view: bool(view._model.feeling_to_part))
-    unblend_from_part: WField = WField(prompt = "If the user feels negative qualities towards the part, help them unblend.", 
+    unblend_from_part: WField = WField(prompt = "If the user feels negative qualities towards the part, ask if that part that feels negative can take a step back during the session so that we can approach from a more compassionate place.", 
                                     skip=lambda view: bool(view._model.achieved_unblending))
-    ask_to_share_feelings: WField = WField(prompt = "If the user confirms the feelings are from Self and they feel positively toward the part, ask them to share their feelings with the part",
+    see_if_more_compassionate: WField = WField(prompt = "Ask how the user feels now that they've unblended from the part.",
+                                skip=lambda view: bool(view._model.has_compassion_towards_part))
+    ask_to_share_feelings: WField = WField(prompt = "If the user confirms the feelings are from Self, ask them to share their feelings with the part",
                                 skip=lambda view: bool(view._model.shared_feelings))
     ask_how_receieved: WField = WField(prompt = "Ask how the part received the shared feelings.")
-
+    
     def __init__(self, model, **data):
         super().__init__(**data)
         self._model = model
